ec18a1ce89aeeb1b038d1e73102e432da9b45eb4
Anjan Roy <hello@itzmeanjan.in>
2022-05-03 09:18:21 +0530

    added diffusion layer `ℒ4`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/sparkle.hpp
+++ include/sparkle.hpp
@@ -34,4 +34,45 @@ alzette(const uint32_t x, const uint32_t y)
   return std::make_pair(lw, rw);
 }
 
+// Diffusion Layer `ℒ4`, used when branch count = 4 i.e. permutation state
+// is 256 ( = 32 * (4 * 2) ) -bit wide
+//
+// See algorithm 2.5 of Sparkle Specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/sparkle-spec-final.pdf
+static inline void
+diffusion_layer_4(uint32_t* const state)
+{
+  // feistel round
+
+  uint32_t tx = state[0] ^ state[2];
+  uint32_t ty = state[1] ^ state[3];
+
+  tx = std::rotl(tx ^ (tx << 16), 16);
+  ty = std::rotl(ty ^ (ty << 16), 16);
+
+  state[5] = state[5] ^ state[1] ^ tx;
+  state[7] = state[7] ^ state[3] ^ tx;
+
+  state[4] = state[4] ^ state[0] ^ ty;
+  state[6] = state[6] ^ state[2] ^ ty;
+
+  // branch permutation
+
+  uint32_t t0 = state[4];
+  uint32_t t1 = state[6];
+
+  state[4] = state[0];
+  state[6] = state[2];
+  state[0] = t1;
+  state[2] = t0;
+
+  t0 = state[5];
+  t1 = state[7];
+
+  state[5] = state[1];
+  state[7] = state[3];
+  state[1] = t1;
+  state[3] = t0;
+}
+
 }
