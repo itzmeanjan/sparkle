359cb4892176f46c16234b58a8075ed5b8a1ae07
Anjan Roy <hello@itzmeanjan.in>
2022-05-03 09:29:07 +0530

    added diffusion layer function `ℒ6`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/sparkle.hpp
+++ include/sparkle.hpp
@@ -75,4 +75,53 @@ diffusion_layer_4(uint32_t* const state)
   state[3] = t0;
 }
 
+// Diffusion Layer `ℒ6`, used when branch count = 6 i.e. permutation state
+// is 384 ( = 32 * (6 * 2) ) -bit wide
+//
+// See algorithm 2.6 of Sparkle Specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/sparkle-spec-final.pdf
+static inline void
+diffusion_layer_6(uint32_t* const state)
+{
+  // feistel round
+
+  uint32_t tx = state[0] ^ state[2] ^ state[4];
+  uint32_t ty = state[1] ^ state[3] ^ state[5];
+
+  tx = std::rotl(tx ^ (tx << 16), 16);
+  ty = std::rotl(ty ^ (ty << 16), 16);
+
+  state[7] = state[7] ^ state[1] ^ tx;
+  state[9] = state[9] ^ state[3] ^ tx;
+  state[11] = state[11] ^ state[5] ^ tx;
+
+  state[6] = state[6] ^ state[0] ^ ty;
+  state[8] = state[8] ^ state[2] ^ ty;
+  state[10] = state[10] ^ state[4] ^ ty;
+
+  // branch permutation
+
+  uint32_t t0 = state[6];
+  uint32_t t1 = state[8];
+  uint32_t t2 = state[10];
+
+  state[6] = state[0];
+  state[8] = state[2];
+  state[10] = state[4];
+  state[0] = t1;
+  state[2] = t2;
+  state[4] = t0;
+
+  t0 = state[7];
+  t1 = state[9];
+  t2 = state[11];
+
+  state[7] = state[1];
+  state[9] = state[3];
+  state[11] = state[5];
+  state[1] = t1;
+  state[3] = t2;
+  state[5] = t0;
+}
+
 }
