9dc0f4b770b8cb647b1a5f1158bf446d1099f615
Anjan Roy <hello@itzmeanjan.in>
2022-05-20 14:34:17 +0530

    updated KAT based testing instructions
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test_kat.sh
+++ test_kat.sh
@@ -18,6 +18,7 @@ unzip sparkle.zip
 # copy Known Answer Tests outside of uncompressed NIST LWC submission directory
 cp sparkle/Implementations/crypto_hash/esch256v2/LWC_HASH_KAT_256.txt ../
 cp sparkle/Implementations/crypto_hash/esch384v2/LWC_HASH_KAT_384.txt ../
+cp sparkle/Implementations/crypto_aead/schwaemm256128v2/LWC_AEAD_KAT_128_256.txt ../
 
 popd
 
@@ -29,6 +30,7 @@ rm -rf tmp
 # move Known Answer Tests to execution directory
 mv LWC_HASH_KAT_256.txt wrapper/python/
 mv LWC_HASH_KAT_384.txt wrapper/python/
+mv LWC_AEAD_KAT_128_256.txt wrapper/python/
 
 # ---
 
