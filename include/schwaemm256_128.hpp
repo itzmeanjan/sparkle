9d3607509c1bccca90bf1680e73c424fc33e4778
Anjan Roy <hello@itzmeanjan.in>
2022-05-20 09:32:16 +0530

    process plain text bytes for Schwaemm256-128 encryption algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/schwaemm256_128.hpp
+++ include/schwaemm256_128.hpp
@@ -15,6 +15,16 @@ constexpr uint32_t CONST_A0 = (0u ^ (1u << 2)) << 24;
 // associated data block
 constexpr uint32_t CONST_A1 = (1u ^ (1u << 2)) << 24;
 
+// To distinguish padded plain text block from non-padded one, this constant is
+// XORed into inner part of permutation state, when processing last plain text
+// block
+constexpr uint32_t CONST_M0 = (2u ^ (1u << 2)) << 24;
+
+// To distinguish non-padded plain text block from padded one, this constant is
+// XORed into inner part of permutation state, when processing last plain text
+// block
+constexpr uint32_t CONST_M1 = (3u ^ (1u << 2)) << 24;
+
 // Initialize permutation state by consuming 16 -bytes secret key & 32 -bytes
 // public message nonce, when performing Schwaemm256-128 authenticated
 // encryption/ verified decryption
@@ -216,6 +226,119 @@ process_associated_data(
   rho1(state, buffer0);
 
   constexpr uint32_t consts[2] = { CONST_A1, CONST_A0 };
+  state[11] ^= consts[rb_full_words < 8];
+
+  omega(state + 8, buffer1);
+
+  for (size_t i = 0; i < (RATE >> 2); i++) {
+    state[i] ^= buffer1[i];
+  }
+
+  sparkle::sparkle<6ul, 11ul>(state);
+}
+
+// Consumes non-empty plain text data into 384 -bit permutation state, while
+// producing equal many cipher text bytes, using algorithm 2.13 of Sparkle
+// specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/sparkle-spec-final.pdf
+static inline void
+process_plain_text(
+  uint32_t* const __restrict state,    // 384 -bit permutation state
+  const uint8_t* const __restrict txt, // N (>0) -bytes plain text
+  uint8_t* const __restrict enc,       // N (>0) -bytes encrypted text
+  const size_t ct_len                  // len(txt) = len(enc) = N | N > 0
+)
+{
+  constexpr size_t RATE = 32; // bytes
+
+  uint32_t buffer0[9];
+  uint32_t buffer1[8];
+  uint32_t buffer2[8];
+
+  size_t r_bytes = ct_len;
+  while (r_bytes > RATE) {
+    const size_t b_off = ct_len - r_bytes;
+
+    for (size_t i = 0; i < (RATE >> 2); i++) {
+      const size_t i_off = i << 2;
+
+      buffer0[i] = (static_cast<uint32_t>(txt[b_off + (i_off ^ 3)]) << 24) |
+                   (static_cast<uint32_t>(txt[b_off + (i_off ^ 2)]) << 16) |
+                   (static_cast<uint32_t>(txt[b_off + (i_off ^ 1)]) << 8) |
+                   (static_cast<uint32_t>(txt[b_off + (i_off ^ 0)]) << 0);
+    }
+
+    std::memcpy(buffer2, state, RATE);
+    rho2(buffer2, buffer0);
+
+    for (size_t i = 0; i < (RATE >> 2); i++) {
+      const size_t i_off = i << 2;
+
+      enc[b_off + (i_off ^ 0)] = static_cast<uint8_t>(buffer2[i] >> 0);
+      enc[b_off + (i_off ^ 1)] = static_cast<uint8_t>(buffer2[i] >> 8);
+      enc[b_off + (i_off ^ 2)] = static_cast<uint8_t>(buffer2[i] >> 16);
+      enc[b_off + (i_off ^ 3)] = static_cast<uint8_t>(buffer2[i] >> 24);
+    }
+
+    rho1(state, buffer0);
+    omega(state + 8, buffer1);
+
+    for (size_t i = 0; i < (RATE >> 2); i++) {
+      state[i] ^= buffer1[i];
+    }
+
+    sparkle::sparkle<6ul, 7ul>(state);
+
+    r_bytes -= RATE;
+  }
+
+  const size_t b_off = ct_len - r_bytes;
+  const size_t rb_full_words = r_bytes >> 2;
+  const size_t rb_rem_bytes = r_bytes & 3ul;
+  const size_t w_off = rb_full_words << 2;
+
+  std::memset(buffer0, 0, RATE);
+
+  for (size_t i = 0; i < rb_full_words; i++) {
+    const size_t i_off = i << 2;
+
+    buffer0[i] = (static_cast<uint32_t>(txt[b_off + (i_off ^ 3)]) << 24) |
+                 (static_cast<uint32_t>(txt[b_off + (i_off ^ 2)]) << 16) |
+                 (static_cast<uint32_t>(txt[b_off + (i_off ^ 1)]) << 8) |
+                 (static_cast<uint32_t>(txt[b_off + (i_off ^ 0)]) << 0);
+  }
+
+  uint32_t word = 0x80u << (rb_rem_bytes << 3);
+  for (size_t i = 0; i < rb_rem_bytes; i++) {
+    const size_t idx = b_off + w_off + i;
+
+    word |= static_cast<uint32_t>(txt[idx]) << (i << 3);
+  }
+
+  const uint32_t words[2] = { 0u, word };
+  buffer0[rb_full_words] = words[rb_full_words < 8];
+
+  std::memcpy(buffer2, state, RATE);
+  rho2(buffer2, buffer0);
+
+  for (size_t i = 0; i < rb_full_words; i++) {
+    const size_t i_off = i << 2;
+
+    enc[b_off + (i_off ^ 0)] = static_cast<uint8_t>(buffer2[i] >> 0);
+    enc[b_off + (i_off ^ 1)] = static_cast<uint8_t>(buffer2[i] >> 8);
+    enc[b_off + (i_off ^ 2)] = static_cast<uint8_t>(buffer2[i] >> 16);
+    enc[b_off + (i_off ^ 3)] = static_cast<uint8_t>(buffer2[i] >> 24);
+  }
+
+  for (size_t i = 0; i < rb_rem_bytes; i++) {
+    const size_t idx = b_off + w_off + i;
+
+    enc[idx] = static_cast<uint8_t>(buffer2[rb_full_words] >> (i << 3));
+  }
+
+  rho1(state, buffer0);
+
+  constexpr uint32_t consts[2] = { CONST_M1, CONST_M0 };
   state[11] ^= consts[rb_full_words < 8];
 
   omega(state + 8, buffer1);
