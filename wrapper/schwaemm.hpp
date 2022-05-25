4d47f519c42194f61b2b2c579df60a36c4f597b8
Anjan Roy <hello@itzmeanjan.in>
2022-05-25 08:58:29 +0400

    C wrapper on top of Schwaemm192-192 AEAD routines
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- wrapper/schwaemm.hpp
+++ wrapper/schwaemm.hpp
@@ -1,10 +1,11 @@
 #pragma once
+#include "schwaemm192_192.hpp"
 #include "schwaemm256_128.hpp"
 
-// Thin C wrapper on top of underlying C++ implementation of Schwaemm256-128
-// AEAD ( authenticated encryption with associated data ) function, which can be
-// used for producing shared library object with C-ABI & used from other
-// languages such as Rust, Python
+// Thin C wrapper on top of underlying C++ implementation of Schwaemm256-128,
+// Schwaemm192-192 AEAD ( authenticated encryption with associated data )
+// function, which can be used for producing shared library object with C-ABI &
+// used from other languages such as Rust, Python
 
 // Function prototype
 extern "C"
@@ -19,6 +20,24 @@ extern "C"
                                uint8_t* const __restrict);
 
   bool schwaemm256_128_decrypt(const uint8_t* const __restrict,
+                               const uint8_t* const __restrict,
+                               const uint8_t* const __restrict,
+                               const uint8_t* const __restrict,
+                               const size_t,
+                               const uint8_t* const __restrict,
+                               uint8_t* const __restrict,
+                               const size_t);
+
+  void schwaemm192_192_encrypt(const uint8_t* const __restrict,
+                               const uint8_t* const __restrict,
+                               const uint8_t* const __restrict,
+                               const size_t,
+                               const uint8_t* const __restrict,
+                               uint8_t* const __restrict,
+                               const size_t,
+                               uint8_t* const __restrict);
+
+  bool schwaemm192_192_decrypt(const uint8_t* const __restrict,
                                const uint8_t* const __restrict,
                                const uint8_t* const __restrict,
                                const uint8_t* const __restrict,
@@ -62,6 +81,40 @@ extern "C"
                                const size_t ct_len)
   {
     using namespace schwaemm256_128;
+    return decrypt(key, nonce, tag, data, d_len, enc, dec, ct_len);
+  }
+
+  // Given 24 -bytes secret key, 24 -bytes nonce, N -bytes plain text & M -bytes
+  // associated data, this routine computes N -bytes cipher text & 24 -bytes
+  // authentication tag | N, M >= 0
+  void schwaemm192_192_encrypt(const uint8_t* const __restrict key,
+                               const uint8_t* const __restrict nonce,
+                               const uint8_t* const __restrict data,
+                               const size_t d_len,
+                               const uint8_t* const __restrict txt,
+                               uint8_t* const __restrict enc,
+                               const size_t ct_len,
+                               uint8_t* const __restrict tag)
+  {
+    schwaemm192_192::encrypt(key, nonce, data, d_len, txt, enc, ct_len, tag);
+  }
+
+  // Given 24 -bytes secret key, 24 -bytes nonce, 24 -bytes authentication tag,
+  // N -bytes cipher text & M -bytes associated data, this routine computes N
+  // -bytes deciphered text & a boolean verification flag | N, M >= 0
+  //
+  // Before consuming decrypted bytes ensure presence of truth value in returned
+  // boolean flag !
+  bool schwaemm192_192_decrypt(const uint8_t* const __restrict key,
+                               const uint8_t* const __restrict nonce,
+                               const uint8_t* const __restrict tag,
+                               const uint8_t* const __restrict data,
+                               const size_t d_len,
+                               const uint8_t* const __restrict enc,
+                               uint8_t* const __restrict dec,
+                               const size_t ct_len)
+  {
+    using namespace schwaemm192_192;
     return decrypt(key, nonce, tag, data, d_len, enc, dec, ct_len);
   }
 }
