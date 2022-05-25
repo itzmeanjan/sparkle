dced6d798c6583027788e29e86da5db37fa29f71
Anjan Roy <hello@itzmeanjan.in>
2022-05-25 07:30:20 +0400

    schwaemm192-192 initialization
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/schwaemm192_192.hpp
+++ include/schwaemm192_192.hpp
@@ -24,4 +24,35 @@ constexpr uint32_t CONST_M0 = (2u ^ (1u << 3)) << 24;
 // block
 constexpr uint32_t CONST_M1 = (3u ^ (1u << 3)) << 24;
 
+// Initialize permutation state by consuming 24 -bytes secret key & 24 -bytes
+// public message nonce, when performing Schwaemm192-192 authenticated
+// encryption/ verified decryption
+//
+// See algorithm 2.15 in Sparkle specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/sparkle-spec-final.pdf
+static inline void
+initialize(uint32_t* const __restrict state,     // 384 -bit permutation state
+           const uint8_t* const __restrict key,  // 24 -bytes secret key
+           const uint8_t* const __restrict nonce // 24 -bytes nonce
+)
+{
+  for (size_t i = 0; i < 6; i++) {
+    const size_t b_off = i << 2;
+    const size_t s_idx0 = i;
+    const size_t s_idx1 = 6ul + i;
+
+    state[s_idx0] = (static_cast<uint32_t>(nonce[b_off ^ 3]) << 24) |
+                    (static_cast<uint32_t>(nonce[b_off ^ 2]) << 16) |
+                    (static_cast<uint32_t>(nonce[b_off ^ 1]) << 8) |
+                    (static_cast<uint32_t>(nonce[b_off ^ 0]) << 0);
+
+    state[s_idx1] = (static_cast<uint32_t>(key[b_off ^ 3]) << 24) |
+                    (static_cast<uint32_t>(key[b_off ^ 2]) << 16) |
+                    (static_cast<uint32_t>(key[b_off ^ 1]) << 8) |
+                    (static_cast<uint32_t>(key[b_off ^ 0]) << 0);
+  }
+
+  sparkle::sparkle<6ul, 11ul>(state);
+}
+
 }
